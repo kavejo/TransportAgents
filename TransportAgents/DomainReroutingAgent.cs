380b461a5fe26b082d481ef7c748d1330fbdf5ef
Tommaso Toniolo <totoni@microsoft.com>
2024-02-12 15:41:47 +0100

    Removed debug logging

--- TransportAgents/DomainReroutingAgent.cs
+++ TransportAgents/DomainReroutingAgent.cs
@@ -31,59 +31,22 @@ namespace TransportAgents
 
         void SendViaCustomRoutingDomain(ResolvedMessageEventSource source, QueuedMessageEventArgs evtMessage)
         {
-            string messageId = evtMessage.MailItem.Message.MessageId.ToString();
-            string sender = evtMessage.MailItem.FromAddress.ToString().ToLower().Trim();
-
-            TextLog.WriteToText("----------------------------------------------------------------------------------------------------");
-            TextLog.WriteToText(String.Format("SendViaCustomRoutingDomain - Processing message: {0} sent from <{1}>", messageId, sender));
-            TextLog.WriteToText("----------------------------------------------------------------------------------------------------");
-
             try
             {
-                Stopwatch redirectionTime = new Stopwatch();
-                redirectionTime.Start();
+                string messageId = evtMessage.MailItem.Message.MessageId.ToString();
+                string sender = evtMessage.MailItem.FromAddress.ToString().ToLower().Trim();
+                string subject = evtMessage.MailItem.Message.Subject.Trim();
 
                 if ( SendersToReroute.Contains(sender) )
                 {
-                    TextLog.WriteToText(String.Format("Rerouting the message as the sender is: {0}", sender));
-
                     foreach (EnvelopeRecipient recipient in evtMessage.MailItem.Recipients)
                     {
                         if (recipient.RecipientCategory == RecipientCategory.InDifferentOrganization || recipient.RecipientCategory == RecipientCategory.Unknown)
                         {
                             source.SetRoutingOverride(recipient, destinationOverride);
-                            TextLog.WriteToText(String.Format("Routing domain overwritten for: {0} as the recipient is external", recipient.Address));
-                        }
-                        else
-                        {
-                            TextLog.WriteToText(String.Format("Routing domain not overwritten for: {0} as the recipient is internal", recipient.Address));
                         }
                     }
-
-                    //foreach (Header header in evtMessage.MailItem.Message.MimeDocument.RootPart.Headers)
-                    //{
-                    //    TextLog.WriteToText(String.Format("Processing: {0}: {1}", header.Name, header.Value));
-                    //    if (header.Value == null || header.Value.Length == 0 || String.IsNullOrEmpty(header.Value))
-                    //    {
-                    //        evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.RemoveChild(header);
-                    //        TextLog.WriteToText(String.Format("Removed as EMPTY"));
-                    //    }
-                    //    if (header.Name == "Received")
-                    //    {
-                    //        evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.RemoveChild(header);
-                    //        TextLog.WriteToText(String.Format("Removed as of type RECEIVED"));
-                    //    }
-                    //}
-
-                    //TextLog.WriteToText("Adding custom headers");
-                    //evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.InsertAfter(new TextHeader("X-TransportAgent-Name", "DomainReroutingAgent"), evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.LastChild);
-                    //evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.InsertAfter(new TextHeader("X-TransportAgent-Creator", "Tommaso Toniolo"), evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.LastChild);
-                    //evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.InsertAfter(new TextHeader("X-TransportAgent-Contact", "https://aka.ms/totoni"), evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.LastChild);
-
                 }
-
-                redirectionTime.Stop();
-                TextLog.WriteToText(String.Format("SendViaCustomRoutingDomain executed for message {0} in {1} ms", messageId, redirectionTime.Elapsed.Milliseconds));
             }
             catch (Exception ex)
             {
@@ -101,54 +64,29 @@ namespace TransportAgents
 
         void RemoveUnsupportedHeaders(CategorizedMessageEventSource source, QueuedMessageEventArgs evtMessage)
         {
-            string messageId = evtMessage.MailItem.Message.MessageId.ToString();
-            string sender = evtMessage.MailItem.FromAddress.ToString().ToLower().Trim();
-
-            TextLog.WriteToText("----------------------------------------------------------------------------------------------------");
-            TextLog.WriteToText(String.Format("RemoveUnsupportedHeaders - Processing message: {0} sent from <{1}>", messageId, sender));
-            TextLog.WriteToText("----------------------------------------------------------------------------------------------------");
-            
             try
             {
-                Stopwatch headerRemovalTime = new Stopwatch();
-                headerRemovalTime.Start();
+                string messageId = evtMessage.MailItem.Message.MessageId.ToString();
+                string sender = evtMessage.MailItem.FromAddress.ToString().ToLower().Trim();
+                string subject = evtMessage.MailItem.Message.Subject.Trim();
 
                 if (SendersToReroute.Contains(sender))
                 {
-                    TextLog.WriteToText(String.Format("Removing unsupported headers the message as the sender is: {0}", sender));
-
                     foreach (Header header in evtMessage.MailItem.Message.MimeDocument.RootPart.Headers)
                     {
-                        TextLog.WriteToText(String.Format("Processing: {0}: {1}", header.Name, header.Value));
                         if (header.Value == null || header.Value.Length == 0 || String.IsNullOrEmpty(header.Value))
                         {
                             if (header.Name != "From" && header.Name != "To")
                             {
                                 evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.RemoveChild(header);
-                                TextLog.WriteToText(String.Format("Removed as EMPTY"));
                             }
-                            else
-                            {
-                                evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.RemoveChild(header);
-                                TextLog.WriteToText(String.Format("Kept EMPTY {0} as it will be updated later", header.Name));
-                            }
-                        }
-                        if (header.Name == "Received")
-                        {
-                            evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.RemoveChild(header);
-                            TextLog.WriteToText(String.Format("Removed as of type RECEIVED"));
                         }
                     }
 
-                    TextLog.WriteToText("Adding custom headers");
                     evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.InsertAfter(new TextHeader("X-TransportAgent-Name", "DomainReroutingAgent"), evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.LastChild);
                     evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.InsertAfter(new TextHeader("X-TransportAgent-Creator", "Tommaso Toniolo"), evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.LastChild);
                     evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.InsertAfter(new TextHeader("X-TransportAgent-Contact", "https://aka.ms/totoni"), evtMessage.MailItem.Message.MimeDocument.RootPart.Headers.LastChild);
-
                 }
-
-                headerRemovalTime.Stop();
-                TextLog.WriteToText(String.Format("RemoveUnsupportedHeaders executed for message {0} in {1} ms", messageId, headerRemovalTime.Elapsed.Milliseconds));
             }
             catch (Exception ex)
             {
