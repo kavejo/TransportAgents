b1c7e4c26bbec80895337af179977f960d50b337
Tommaso Toniolo <totoni@microsoft.com>
2024-10-08 13:21:42 +0200

    Bumped the version on the Assembly Info

--- TransportAgents/Properties/AssemblyInfo.cs
+++ TransportAgents/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TransportAgents")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sample Transport Agents, fully functional and extensively tested, that achieve different business goals by leveraging various Trasnport Agents capabiltiies in Microsoft Exchange Server.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tommaso Toniolo - Microsoft")]
 [assembly: AssemblyProduct("TransportAgents")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
